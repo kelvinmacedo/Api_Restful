cf0652ae5b4c01bab24af304cb6519b949757bed
Kelvin Marques (Max) <92284776+kelvinmacedo@users.noreply.github.com>
2022-07-16 13:19:36 -0300

    refatorado para jpa

--- src/main/java/io/github/kelvinmacedo/repository/ClienteRepositorio.java
+++ src/main/java/io/github/kelvinmacedo/repository/ClienteRepositorio.java
@@ -2,19 +2,56 @@ package io.github.kelvinmacedo.repository;
 
 import io.github.kelvinmacedo.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Repository
 public class ClienteRepositorio {
-
-    private static String INSERT = "insert into cliente (nome) values (?)";
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private EntityManager entityManager;
 
+    @Transactional
     public Cliente salvar(Cliente cliente){
-        jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
+        entityManager.persist(cliente);
         return cliente;
+    }
 
+    @Transactional
+    public  Cliente atualizar(Cliente cliente){
+        entityManager.merge(cliente);
+        return cliente;
+    }
+
+    @Transactional
+    public void deletar(Cliente cliente){
+       if (!entityManager.contains(cliente)){
+           cliente = entityManager.merge(cliente);
+       }
+        entityManager.remove(cliente);
+    }
+
+    @Transactional
+    public void deletar(Integer id){
+        Cliente cliente = entityManager.find(Cliente.class, id);
+        deletar(cliente);
+    }
+
+    @Transactional(readOnly = true)
+    public List <Cliente> pesquisarPorNome(String nome){
+       String jpql = "select c from cliente c where c.nome like :nome";
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("nome", "%" + nome + "%");
+        return query.getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cliente> obterTodos(){
+        return entityManager
+                .createQuery("from cliente", Cliente.class)
+                .getResultList();
     }
 }
