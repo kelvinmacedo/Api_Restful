abe2338ab1168d711db4bda7bd65df7020b8b1a6
Kelvin Marques (Max) <92284776+kelvinmacedo@users.noreply.github.com>
2022-10-23 14:28:26 -0300

    refatorando codigo

--- src/main/java/io/github/kelvinmacedo/repository/ClienteRepositorio.java
+++ src/main/java/io/github/kelvinmacedo/repository/ClienteRepositorio.java
@@ -2,6 +2,7 @@ package io.github.kelvinmacedo.repository;
 
 import io.github.kelvinmacedo.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,5 +13,12 @@ public interface ClienteRepositorio extends JpaRepository<Cliente, Integer> {
     @Query(value = "select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true)
     List<Cliente> encontarPorNome(@Param("nome") String nome);
 
+    @Query(" delete from Cliente c where c.nome =:nome ")
+    @Modifying
+    void deleteByNome(String nome);
 
+    //boolean existsByNome(String nome);
+
+    @Query(" select c from Cliente c left join fetch c.pedidos where c.id = :id  ")
+    Cliente findClienteFetchPedidos( @Param("id") Integer id );
 }
--- src/main/java/io/github/kelvinmacedo/restController/ClienteControle.java
+++ src/main/java/io/github/kelvinmacedo/restController/ClienteControle.java
@@ -2,62 +2,72 @@ package io.github.kelvinmacedo.restController;
 
 import io.github.kelvinmacedo.domain.entity.Cliente;
 import io.github.kelvinmacedo.repository.ClienteRepositorio;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
+import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/clientes")
 public class ClienteControle {
 
-    private ClienteRepositorio clientes;
+    private final ClienteRepositorio clientes;
 
     public ClienteControle(ClienteRepositorio clientes) {
         this.clientes = clientes;
     }
 
-    @GetMapping("/api/cliente/{id}")
-    @ResponseBody
-    public ResponseEntity getClienteById(@PathVariable Integer id){
-        Optional<Cliente> cliente = clientes.findById(id);
-        if(cliente.isPresent()){
-            return ResponseEntity.ok(cliente.get());
-        }
-        return ResponseEntity.notFound().build();
+    @GetMapping("{id}")
+    public Cliente encontarPorNome (@PathVariable Integer id){
+       return clientes
+                .findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                "Cliente não encontrado"));
     }
 
-    @PostMapping("api/cadastrar")
-    @ResponseBody
-    public ResponseEntity save(@RequestBody Cliente cliente ){
-        Cliente salvarCliente = clientes.save(cliente);
-        return ResponseEntity.ok(salvarCliente);
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente save(@RequestBody Cliente cliente ){
+        return clientes.save(cliente);
 
     }
-    @DeleteMapping("/api/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity delete(@PathVariable Integer id){
-        Optional<Cliente> cliente = clientes.findById(id);
-        if (cliente.isPresent()){
-            clientes.delete(cliente.get());
-            return ResponseEntity.noContent().build();
-        }
-
-        return ResponseEntity.notFound().build();
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete( @PathVariable Integer id){
+        clientes.findById(id)
+                .map(cliente -> {clientes.delete(cliente);
+                return cliente;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Cliente nao encontrado"));
     }
 
-    @PutMapping("/api/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity update(@PathVariable Integer id,
-                                 @RequestBody Cliente cliente){
-        return clientes
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id,
+                       @RequestBody Cliente cliente){
+        clientes
                 .findById(id)
                 .map(clienteExistente -> {
                         cliente.setId(clienteExistente.getId());
                         clientes.save(cliente);
-                        return ResponseEntity.noContent().build();
-                }).orElseGet(() -> ResponseEntity.notFound().build());
+                        return clienteExistente;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Cliente não encontrador"));
 
     }
-
+    @GetMapping
+    public List<Cliente> find(Cliente filtro){
+        ExampleMatcher matcher =ExampleMatcher
+                                .matching()
+                                .withIgnoreCase()
+                                .withStringMatcher(
+                                        ExampleMatcher.StringMatcher.CONTAINING);
+        Example<Cliente> example = Example.of(filtro, matcher);
+        return clientes.findAll(example);
+    }
 }
